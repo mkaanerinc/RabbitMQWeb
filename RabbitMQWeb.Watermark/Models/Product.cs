1ad5376aefc6e4e683da3bcd61879e0ffb816118
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-12-30 11:13:27 +0300

    Added Create method to ProductsController & Sent data to RabbitMQ

--- RabbitMQWeb.Watermark/Models/Product.cs
+++ RabbitMQWeb.Watermark/Models/Product.cs
@@ -14,6 +14,6 @@ namespace RabbitMQWeb.Watermark.Models
         [Range(1,100)]
         public int Stock { get; set; }
         [StringLength(100)]
-        public string PictureUrl { get; set; }
+        public string ImageName { get; set; }
     }
 }
